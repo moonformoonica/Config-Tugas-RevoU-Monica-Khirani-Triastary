a37439e4d30e7283375d14269bdacc6cd3eeaea1
Monica Khirani Triastary <monicakhirani273@gmail.com>
2024-07-31 13:51:24 +0700

    Assignment RevoU

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
 /* Ini Javascript */
+
+@media ( max-width: 1200px) {
+    header {
+        display: fle*;
+        flex-direction: column;
+        justify-content: center;
+    }
+    .container {
+        display: flex;
+        flex-direction: column;
+        margin: 0px;
+    }
+    .banner {
+        border-radius: opx;
+    }
+    .slider {
+        border-radius: 0px;
+    }
+    .about {
+        border-radius: 0px;
+    }
+    .order {
+        width: 100%;
+    }
+    .order-container {
+        padding-top: 50px;
+        padding-bottom: 50px;
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        margin-left: 0px;
+        border-radius: 0px;
+    }
+    .orderbg {
+        display: flex;
+        flex-direction: column;
+        backgroundc: linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(../assets/bg/img_main02.jpg);
+        justify-content: center;
+        background-repeat: repeat-x;
+        background-position: 50% 0%;
+        border-radius: 0px;
+    }
+}
